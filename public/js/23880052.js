27a09d921c2e7f934e49c7d34d03284c70288c25
Trung Nguyen Nguyen <aeonamind@gmail.com>
2025-05-26 01:31:13 +0700

    register

--- public/js/23880052.js
+++ public/js/23880052.js
@@ -97,3 +97,17 @@ function placeorders(e) {
 
   e.target.submit();
 }
+
+function checkPasswordConfirm(formId) {
+  let password = document.querySelector(`#${formId} [name=password]`);
+  let confirmPassword = document.querySelector(
+    `#${formId} [name=confirmPassword]`,
+  );
+
+  if (password.value != confirmPassword.value) {
+    confirmPassword.setCustomValidity('Passwords not match!');
+    confirmPassword.reportValidity();
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+}
