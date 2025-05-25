36946984144573f2d3ef9ad580993e1c61817005
Trung Nguyen Nguyen <aeonamind@gmail.com>
2025-05-25 16:40:39 +0700

    second commit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,32 @@
 'use strict';
 
 const controller = {};
+const models = require('../models');
 
-controller.showHomePage = (req, res) => {
-  res.render('index');
+controller.showHomePage = async (req, res) => {
+  const recentProducts = await models.Product.findAll({
+    attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice'],
+    order: [['createdAt', 'DESC']],
+    limit: 10,
+  });
+  res.locals.recentProducts = recentProducts;
+
+  const featuredProducts = await models.Product.findAll({
+    attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice'],
+    order: [['stars', 'DESC']],
+    limit: 10,
+  });
+  res.locals.featuredProducts = featuredProducts;
+
+  const categories = await models.Category.findAll();
+  // [1, 2, 3, 3] => [[1], [3,4], [2]]
+  const secondArray = categories.splice(2, 2);
+  const thirdArray = categories.splice(1, 1);
+  res.locals.categoryArray = [categories, secondArray, thirdArray];
+
+  const Brand = models.Brand;
+  const brands = await Brand.findAll();
+  res.render('index', { brands });
 };
 
 controller.showPage = (req, res, next) => {
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const expressHandlerbars = require('express-handlebars');
+const { createStarList } = require('./controllers/handlebarsHelper');
+const { createPagination } = require('express-handlebars-paginate');
+const session = require('express-session');
 
 // cau hinh static folder
 app.use(express.static(__dirname + '/public'));
@@ -16,12 +19,47 @@ app.engine(
     partialsDir: __dirname + '/views/partials',
     extname: 'hbs',
     defaultLayout: 'layout',
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+    },
+    helpers: {
+      createStarList,
+      createPagination,
+    },
   }),
 );
 app.set('view engine', 'hbs');
 
+// cau hinh doc du lieu post tu body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// cau hinh dung session
+app.use(
+  session({
+    secret: 'aeonamind',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 20 * 60 * 1000, // 20 phut
+    },
+  }),
+);
+
+// middleware khoi tao gio hang
+app.use((req, res, next) => {
+  let Cart = require('./controllers/cart');
+  req.session.cart = new Cart(req.session.cart ? req.session.cart : {});
+  res.locals.quantity = req.session.cart.quantity;
+
+  next();
+});
+
 // routes
 app.use('/', require('./routes/indexRouter'));
+app.use('/products', require('./routes/productsRouter'));
+app.use('/users', require('./routes/usersRouter'));
 
 app.use((req, res) => {
   res.status(404).render('error', { message: 'File not found!' });
